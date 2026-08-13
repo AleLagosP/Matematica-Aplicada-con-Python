8f5e739bd696ab4966849d94f8fd435863d775c1
Alejandro Lagos <134658061+AleLagosP@users.noreply.github.com>
2026-08-13 00:59:09 -0400

    siguiendo con el ejercicio

--- Clase 1 - inrtroduccion/Resultados_Guia1.py
+++ Clase 1 - inrtroduccion/Resultados_Guia1.py
@@ -1,7 +1,7 @@
 # Resultado guia 1 10/08/2026
 
 # Respuesta 1:
-def datos_transmitidos(segundos ):
+def datos_transmitidos(segundos):
     velocidad = 100 
     return velocidad * segundos;
 
@@ -43,4 +43,51 @@ c = calcular_latencia(74) #Milisegundos del tercer caso
 print("Latencia real del primer caso:", a, "ms")
 print("Latencia real del segundo caso:", b, "ms")
 print("Latencia real del tercer caso:", c, "ms")
+
+#Respuesta 4:
+# 1) Determinando la longitud del cable en metros, sabiendo que la velocidad de propagación es de 1.85 m/s y el tiempo de propagación es de 100 segundos.
+def largo_cable(tiempo):
+    return 1.85 * tiempo
+
+print(largo_cable(100)) #100 segundos
+
+# 2) Definicion variable dependiente e independiente, indicando unidad de medida.
+
+# la variable independiente es el tiempo de propagacion, medido en segundos.
+    # T= tiempo de hora 
+# la variable dependiente es el largo del cable, medido en metros.
+    # L(t) = kilometros de cable instalado 
+
+# 3) Determinando el dominio contextuado de la funcion
+
+# Tenemos : 6600 = 1.85 * t
+
+# Despejando t, tenemos que t = 6600 / 1.85
+
+# Resultado: t = 3567.567 segundos
+
+# El dominio es : 0 <= t <= 3567.567 segundos
+
+# 4) Grafico de la funcion, indicando el dominio contextuado
+import matplotlib.pyplot as plt 
+def largo_cable(tiempo):    
+    return 1.85 * tiempo
+
+tiempo_final = 6600 / 1.85
+
+horas = []
+kilometros = []
+
+for t in range(0, int(tiempo_final) + 1):
+    horas.append(t)
+    kilometros.append(largo_cable(t))
+
+plt.plot(horas, kilometros)
+plt.xlabel("Tiempo (horas)")
+plt.ylabel("Cable instalado (km)")
+plt.title("Instalación del cable de fibra óptica")
+plt.grid()
+plt.show()
+
+
 
